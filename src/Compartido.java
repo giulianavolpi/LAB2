d1999a93a7e5c77337e0caffbca74d166257edf8
giulianavolpi <g.volpi@uniandes.edu.co>
2025-01-31 23:20:04 -0500

    Adelanto Punto 1

--- src/Compartido.java
+++ src/Compartido.java
@@ -3,14 +3,12 @@ public class Compartido extends Thread {
     private int suma;
     private int[][] matriz;
 
-    public synchronized void adicionarAcumFila(int matriz) {
-        suma = 0;
-        for (int i = 0; i < matriz; i++) {
-            suma += matriz;
-        }
+    public synchronized void adicionarAcumFila(int sumaFila) {
+
+        suma += sumaFila;
     }
 
-    public synchronized void darTotal(int matriz) {
+    public synchronized void darTotal() {
 
         System.out.println("La suma de la matriz es: " + suma);
     }
--- src/MainP1.java
+++ src/MainP1.java
@@ -1,3 +1,29 @@
+import java.util.Random;
+
 public class MainP1 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Compartido compartido = new Compartido();
+        int n = 100;
+        int[][] matriz = new int[n][n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = rand.nextInt(100);
+            }
+        }
+
+        Thread[] filas = new Thread[n];
+        for (int i = 0; i < n; i++) {
+            boolean esUltimo = false;
+            filas[i] = new Fila(matriz[i], compartido, esUltimo);
+            filas[i].start();
+        }
+
+        // compartido.start();
+
+    }
 
 }
